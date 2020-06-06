363cbe55987ed9854f09ee70e39188045397080e
Vladimir <mr.vladimir.vl1@gmail.com>
2020-06-06 17:00:59 +0300

    creation_folder

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.slider__inner').slick({
+		speed: 1300,
+		prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/left.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/right.png"></button>'
+	  });
+});
